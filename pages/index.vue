aa610ef64ef27e4632e476e30a6256959ed4185c
HalloSouf <soufianesedd@gmail.com>
2023-07-13 19:28:46 +0200

    🔍 Added SEO tags

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,22 @@
+<script setup lang="ts">
+useServerSeoMeta({
+  title: 'Souf IT - Voor jouw IT oplossingen',
+  description: 'Zeg hallo tegen Souf IT. Samen ontdekken wij grenzeloze mogelijkheden voor uw situatie. Doe je mee?',
+  author: 'HalloSouf',
+  charset: 'utf-8',
+  ogDescription: 'Zeg hallo tegen Souf IT. Samen ontdekken wij grenzeloze mogelijkheden voor uw situatie. Doe je mee?',
+  viewport: 'width=device-width, initial-scale=1.0',
+  ogUrl: 'https://soufit.nl',
+  ogTitle: 'Souf IT - Voor jouw IT oplossingen',
+  ogSiteName: 'Souf IT',
+  ogType: 'website'
+});
+</script>
+
 <template>
   <div>
-    <!-- <Navbar /> -->
-
     <div>
       <LandingSection />
-
-      <!-- <AboutMeSection />
-
-      <div class="h-screen" /> -->
     </div>
   </div>
 </template>

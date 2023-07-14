3c09707fbac429ebeeae32c95344ba03350d0813
HalloSouf <soufianesedd@gmail.com>
2023-07-14 10:07:15 +0200

    ⛰️ Added animation to scroll top button

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { TransitionRoot } from '@headlessui/vue';
+
 const { $gsap } = useNuxtApp();
 
 const scrollPosition = ref<number>(0);
@@ -18,10 +20,18 @@ useServerSeoMeta({
   ogType: 'website'
 });
 
-const scrollToTop = () => {
-  $gsap.to(window, { duration: 0.5, scrollTo: 0 });
+/**
+ * Scrolls the window to the top of the page using GSAP.
+ * @returns {GSAPTween} - The GSAP tween object.
+ */
+const scrollToTop = (): GSAPTween => {
+  return $gsap.to(window, { duration: 0.5, scrollTo: 0 });
 };
 
+/**
+ * Sets up a scroll event listener to update the scroll position.
+ * @returns {void}
+ */
 onMounted((): void => {
   window.addEventListener('scroll', () => {
     scrollPosition.value = window.scrollY;
@@ -41,10 +51,24 @@ onMounted((): void => {
       <AboutMeSection />
     </div>
 
-    <div v-if="scrollPosition > 10" class="fixed bottom-2 right-5">
-      <button class="bg-sky-900 h-10 w-10 rounded-lg shadow-md text-white" @click="() => scrollToTop()">
-        <font-awesome-icon :icon="['fas', 'mountain-sun']" />
-      </button>
+    <div class="fixed bottom-5 right-10">
+      <TransitionRoot
+        :show="scrollPosition > 10"
+        as="template"
+        enter="duration-500 transition"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-500 transition"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <button
+          class="bg-sky-900 h-10 w-10 rounded-lg shadow-md text-white"
+          @click="() => scrollToTop()"
+        >
+          <font-awesome-icon :icon="['fas', 'arrow-up']" />
+        </button>
+      </TransitionRoot>
     </div>
   </div>
 </template>

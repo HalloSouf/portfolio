c1c3cf72d53c037b7238bc907ffc49b66dbf6807
HalloSouf <soufianesedd@gmail.com>
2023-07-21 16:14:37 +0200

    ⛷️ Fixed no config bug

--- components/footer.vue
+++ components/footer.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+</script>
+
 <template>
   <footer>
     <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
